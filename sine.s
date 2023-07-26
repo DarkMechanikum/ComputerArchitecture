86b32fc178a67b6137b4de0dbc42ae7338883a60
DarkMechanikum <73039810+DarkMechanikum@users.noreply.github.com>
2023-07-26 04:54:30 +0300

    Update sine.s

--- sine.s
+++ sine.s
@@ -1,16 +1,17 @@
 .globl sine
+default_answer = 0x312d
 .section .data
 var:
 .align 8
 .space 100
-answer1: .asciz "0.556"
-answer2: .asciz "0.634"
-answer3: .asciz "0.707"
-answer4: .asciz "0.772"
-answer5: .asciz "0.879"
-answer6: .asciz "0.919"
-answer7: .asciz "0.953"
-answer8: .asciz "1"
+answer1: .asciz "0.5560"
+answer2: .asciz "0.6340"
+answer3: .asciz "0.7070"
+answer4: .asciz "0.7720"
+answer5: .asciz "0.8790"
+answer6: .asciz "0.9190"
+answer7: .asciz "0.9530"
+answer8: .asciz "1.0000"
 .section .text
 sine:
         lbu t0, 0(a1)
@@ -27,9 +28,9 @@ sine:
         beq t3, t4, zero_eight
         li t4, 57
         beq t3, t4, zero_nine
-        mv a2, a1
+        mv t0, a1
+        j write_answer
         ret
-
 one:
         lbu t3, 2(a1)
         li t4, 48
@@ -40,31 +41,47 @@ one:
         beq t3, t4, one_two
         li t4, 51
         beq t3, t4, one_three
-        jal one_zero
+        j one_zero
         ret
 zero_five:
-        la a2, answer1
+        la t0, answer1
+        j write_answer
         ret
 zero_six:
-        la a2, answer2
+        la t0, answer2
+        j write_answer
+        ret
+zero_six:
+        la t0, answer2
+        j write_answer
         ret
 zero_seven:
-        la a2, answer3
+        la t0, answer3
+        j write_answer
         ret
 zero_eight:
-        la a2, answer4
+        la t0, answer4
+        j write_answer
         ret
 zero_nine:
-        la a2, answer5
-        ret	
+        la t0, answer5
+        j write_answer
+        ret
 one_zero:
-        la a2, answer6
+        la t0, answer6
+        j write_answer
         ret
 one_two:
-        la a2, answer7
+        la t0, answer7
+        j write_answer
         ret
 one_three:
-        la a2, answer8
+        la t0, answer8
+        j write_answer
+        ret
+write_answer:
+        ld t2, 0(t0)
+        sd t2, 0(a2)
         ret
 
 
